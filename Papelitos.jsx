95efbbef685aa1edd1d82609c7c88761c3b93aa0
Danny Pastolero <danpastolero@gmail.com>
2026-08-22 11:30:39 +0800

    fix(voucher): remove top margin from printable cash vouchers

--- Papelitos.jsx
+++ Papelitos.jsx
@@ -1197,7 +1197,7 @@ export default function Papelitos() {
       const voucherW = 6.5 * 72; // 468 pt (6.5 inches)
       const voucherH = 2.5 * 72; // 180 pt (2.5 inches height)
       const boxX = (a4Width - voucherW) / 2; // 63.64 pt (centered horizontally)
-      const marginTop = 0.2 * 72; // 14.4 pt (.2 inch margin on top)
+      const marginTop = 0; // 0 pt (No top margin)
 
       const voucherItems = Array.isArray(recordsToVoucher) ? recordsToVoucher : [recordsToVoucher];
 
